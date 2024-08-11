458a93251836f46301c74405866a87224769f70a
Yago de Matos <contato.yagodematos@gmail.com>
2024-08-11 12:45:53 -0300

    Fix formating of python files

--- beecrowd/iniciante/1115.py
+++ beecrowd/iniciante/1115.py
@@ -3,13 +3,12 @@ x, y = [int(i) for i in input().split()]
 while x != 0 and y != 0:
 
     if y > 0 and x > 0:
-        print('primeiro')
+        print("primeiro")
     elif y > 0 and x < 0:
-        print('segundo')
+        print("segundo")
     elif y < 0 and x < 0:
-        print('terceiro')
+        print("terceiro")
     else:
-        print('quarto')
+        print("quarto")
 
     x, y = [int(i) for i in input().split()]
-
--- beecrowd/iniciante/1173.py
+++ beecrowd/iniciante/1173.py
@@ -9,4 +9,3 @@ for i in range(10):
 
 for i in range(10):
     print(f"N[{i}] = {N[i]}")
-
