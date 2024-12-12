1f6b531b6fb2a958683d7dc4c2a4de27f852e8e4
Yago de Matos <contato.yagodematos@gmail.com>
2024-12-12 03:21:39 -0300

    add diferent solution to beecrowd 1098

--- beecrowd/iniciante/1098.py
+++ beecrowd/iniciante/1098.py
@@ -1,6 +1,13 @@
-for i in [x / 10.0 for x in range(0, 21, 2)]:
-    for j in range(3):
-        if i == 0.0 or i == 1.0 or i == 2.0:
-            i = int(i)
+# for i in [x / 10.0 for x in range(0, 21, 2)]:
+    # for j in range(3):
+        # if i == 0.0 or i == 1.0 or i == 2.0:
+            # i = int(i)
 
-        print(f"I={i} J={j+1+i}")
+        # print(f"I={i} J={j+1+i}")
+
+for i in [x / 10.0 for x in range(0, 21, 2)]:
+    for j in range(1, 4):
+        if i.is_integer():
+            print(f"I={int(i)} J={int(j+i)}")
+        else:
+            print(f"I={i} J={j+i}")
